22e2e1199f588c0ff58d978ad7a4758de7427258
Demo <demotascha@gmail.com>
2020-02-23 15:49:12 +0800

    refactory: Update coding style

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 
 class MainHandler(RequestHandler):
-    async def get(self):
-        self.write({'message': 'hello world'})
+  async def get(self):
+    self.write({'message': 'hello world'})
 
 
 def make_app():
-  urls = [("/", MainHandler)]
-  return Application(urls)
-
+  urls = [
+      ("/", MainHandler)
+  ]
+  return Application(urls, debug=True)
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8080)
-    IOLoop.instance().start()
+  app = make_app()
+  app.listen(8080)
+  IOLoop.instance().start()
