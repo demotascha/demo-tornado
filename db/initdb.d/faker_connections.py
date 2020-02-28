baaeab66b82be6f206711cc6479d50e158414285
Demo <demotascha@gmail.com>
2020-02-28 16:42:07 +0800

    refactory: 移動檔案至 faker*.py 從 `db/initdb.d/` 到 `db/faker_init/`

--- db/initdb.d/faker_connections.py
+++
@@ -1,8 +0,0 @@
-from faker import Faker
-import random
-
-faker = Faker()
-
-for num in range(1, 10):
-  print(
-      f'INSERT INTO connections (follower_id, followee_id, create_at) VALUES ({num}, {random.randint(1,10)}, NOW());')
