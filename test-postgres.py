0e56d29ce6d763f8dccfe452d05489476d727c4b
Demo <demotascha@gmail.com>
2020-02-28 16:45:58 +0800

    fix: 測試取出資料

--- test-postgres.py
+++ test-postgres.py
@@ -1,25 +1,52 @@
+import json
 from psycopg2 import connect
 
 table_name = "tweets"
 
 # declare connection instance
 conn = connect(
-    dbname = "postgres",
+    dbname="postgres",
     user="postgres",
-    host = "db",
+    host="db",
     password="mysecretpassword"
 )
 
 # declare a cursor object from the connection
 cursor = conn.cursor()
 
+tweets = []
+replies = []
+favorites = []
 # execute an SQL statement using the psycopg2 cursor object
-cursor.execute(f"SELECT * FROM {table_name};")
-
-# enumerate() over the PostgreSQL records
+cursor.execute(
+    f"SELECT tweet_id, tweet_txt FROM {table_name};")
 for i, record in enumerate(cursor):
-    print ("\n", type(record))
-    print(record)
+    tweet = {}
+    tweet["tweet_id"] = record[0]
+    tweet["tweet_txt"] = record[1]
+    tweets.append(tweet)
+
+# replies
+cursor.execute(
+    f"SELECT user_id, tweet_id, reply_text, create_at FROM reply;")
+for i, record in enumerate(cursor):
+    reply = {}
+    reply["user_id"] = record[0]
+    reply["tweet_id"] = record[1]
+    reply["reply_text"] = record[2]
+    replies.append(reply)
+
+# favorites
+cursor.execute(
+    f"SELECT user_id, tweet_id, create_at FROM favorites;")
+for i, record in enumerate(cursor):
+    favorite = {}
+    favorite["user_id"] = record[0]
+    favorite["tweet_id"] = record[1]
+    favorites.append(favorite)
+
+print({'tweets': json.dumps(tweets), 'reply': json.dumps(
+    replies), 'favorites': json.dumps(favorites)})
 
 # close the cursor object to avoid memory leaks
 cursor.close()
